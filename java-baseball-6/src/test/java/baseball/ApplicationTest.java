fc5be5ce7ea00cc202654ffce552052c7e7fde51
Sanghee0820 <lsh277604@gmail.com>
2024-01-12 19:14:40 +0900

    test: 올바른값 중복값 입력

--- java-baseball-6/src/test/java/baseball/ApplicationTest.java
+++ java-baseball-6/src/test/java/baseball/ApplicationTest.java
@@ -5,6 +5,7 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import baseball.domain.Computer;
 import baseball.service.ValueGenerator;
 import baseball.type.Result;
 import camp.nextstep.edu.missionutils.test.NsTest;
@@ -58,6 +59,49 @@ class ApplicationTest extends NsTest {
         }
     }
 
+    @Test
+    void 입력값으로_결과값_생성_테스트(){
+        List<Integer> computerValue = Arrays.asList(1,2,3);
+        Computer computer = new Computer(computerValue);
+        List<List<Integer>> humanValue = Arrays.asList(
+                Arrays.asList(1,2,3),
+                Arrays.asList(2,3,6),
+                Arrays.asList(3,2,1),
+                Arrays.asList(1,4,5),
+                Arrays.asList(5,6,7)
+        );
+        List<Result> expectedResult = Arrays.asList(
+                Result.B0S3,
+                Result.B2,
+                Result.B2S1,
+                Result.B0S1,
+                Result.NOTHING
+        );
+        for(int testIndex = 0; testIndex < 5; testIndex++){
+            try {
+                assertThat(computer.compareValue(humanValue.get(testIndex)))
+                        .isEqualTo(expectedResult.get(testIndex));
+            }catch (Exception e){
+                System.out.println("wrong");
+            }
+        }
+    }
+    @Test
+    void 중복값_입력_테스트(){
+        List<Integer> computerValue = Arrays.asList(1,2,3);
+        Computer computer = new Computer(computerValue);
+        List<List<Integer>> humanValues = Arrays.asList(
+                Arrays.asList(1,1,2),
+                Arrays.asList(2,2,3),
+                Arrays.asList(3,3,3),
+                Arrays.asList(1,5,5),
+                Arrays.asList(5,7,7)
+        );
+        for(List<Integer> humanValue: humanValues){
+            assertThatThrownBy(() -> computer.compareValue(humanValue))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
     @Override
     public void runMain() {
         Application.main(new String[]{});
