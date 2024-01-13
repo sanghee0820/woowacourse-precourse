14232cfd9bdc66a64406c59a90f954128eaa231f
Sanghee0820 <lsh277604@gmail.com>
2024-01-13 13:49:10 +0900

    refactor: 클래스를 final로 변경

--- java-baseball-6/src/main/java/baseball/service/ValueGenerator.java
+++ java-baseball-6/src/main/java/baseball/service/ValueGenerator.java
@@ -4,8 +4,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ValueGenerator {
-    public static List<Integer> getRandomValue(){
+public final class ValueGenerator {
+    public static List<Integer> getRandomValue() {
         List<Integer> value = new ArrayList<>();
         while (value.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
