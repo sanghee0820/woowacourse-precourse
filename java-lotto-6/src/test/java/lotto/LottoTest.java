bc45339032decd50e5367365ad15a6ab02e0b981
sanghee0820 <lsh277604@gmail.com>
2024-01-26 10:36:48 +0900

    test: Lotto 결과 비교 후 결과값 반환테스트

--- java-lotto-6/src/test/java/lotto/LottoTest.java
+++ java-lotto-6/src/test/java/lotto/LottoTest.java
@@ -1,12 +1,14 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.ArrayList;
+import java.util.List;
 import lotto.model.Lotto;
+import lotto.model.Rank;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -24,5 +26,21 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호 확인 테스트")
+    @Test
+    void compareLottoNumTest() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 8));
+        List<List<Integer>> normalNumbers = new ArrayList<>();
+        normalNumbers.add(List.of(1, 2, 3, 9, 10, 11));
+        normalNumbers.add(List.of(1, 2, 3, 4, 10, 11));
+        normalNumbers.add(List.of(1, 2, 3, 4, 5, 11));
+        normalNumbers.add(List.of(1, 2, 3, 4, 5, 8));
+
+        List<Integer> bonusNumbers = List.of(15, 5, 10, 11);
+        List<Rank> result = List.of(Rank.FIFTH_RANK, Rank.FORTH_RANK, Rank.THIRD_RANK, Rank.FIRST_RANK);
+        for (int i = 0; i < normalNumbers.size(); i++) {
+            assertThat(lotto.compare(normalNumbers.get(i), bonusNumbers.get(i))).isEqualTo(result.get(i));
+        }
+
+    }
 }
