272c27c71830ecea89ae3d27923f376318135d67
Sanghee0820 <lsh277604@gmail.com>
2024-02-11 21:42:10 +0900

    feat: 주말/주중 확인

--- java-christmas-6/src/main/java/christmas/domain/constant/Day.java
+++ java-christmas-6/src/main/java/christmas/domain/constant/Day.java
@@ -8,4 +8,10 @@ public enum Day {
         return Day.values()[LocalDate.of(2023, 12, day).getDayOfWeek().getValue() - 1];
     }
 
+    public boolean isWeekend(){
+        return this.equals(FRIDAY) || this.equals(SATURDAY);
+    }
+    public boolean isWeekDay(){
+        return !isWeekend();
+    }
 }
