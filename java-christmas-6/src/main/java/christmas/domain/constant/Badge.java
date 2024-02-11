12b8c6729031509db044f2281d27da412ddda497
Sanghee0820 <lsh277604@gmail.com>
2024-02-11 21:42:27 +0900

    feat: enum - 뱃지 추가

---
+++ java-christmas-6/src/main/java/christmas/domain/constant/Badge.java
@@ -0,0 +1,23 @@
+package christmas.domain.constant;
+
+public enum Badge {
+    NONE("없음"), STAR("별"), TREE("트리"), SANTA("산타");
+
+    private final String desc;
+
+    Badge(String desc){
+        this.desc = desc;
+    }
+    public Badge of(int discount){
+        if(discount < 5000){
+            return NONE;
+        }
+        if(discount < 10000){
+            return STAR;
+        }
+        if(discount < 20000){
+            return TREE;
+        }
+        return SANTA;
+    }
+}
