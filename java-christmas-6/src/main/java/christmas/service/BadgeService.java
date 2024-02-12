b628bc114c05b7da194400fd5d4b4bcebad5f0e6
sanghee0820 <lsh277604@gmail.com>
2024-02-12 16:08:56 +0900

    feat: getBadge 추가

--- java-christmas-6/src/main/java/christmas/service/BadgeService.java
+++ java-christmas-6/src/main/java/christmas/service/BadgeService.java
@@ -1,8 +1,15 @@
 package christmas.service;
 
+import christmas.domain.Visitor;
+import christmas.domain.constant.Badge;
 import christmas.domain.policy.badgePolicy.BadgePolicy;
 import christmas.domain.policy.badgePolicy.BadgePolicyImpl;
+import java.util.Map;
 
 public class BadgeService {
     private final BadgePolicy badgePolicy = new BadgePolicyImpl();
+
+    public Badge getBadge(Map<String, Integer> events){
+        return badgePolicy.getBadge(events.values().stream().mapToInt(Integer::intValue).sum());
+    }
 }
