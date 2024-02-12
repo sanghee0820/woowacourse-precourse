dd427cdcd0d29997050ad5a086c6a478ed37aa55
sanghee0820 <lsh277604@gmail.com>
2024-02-12 10:55:06 +0900

    feat: VisitDay 클래스 추가

---
+++ java-christmas-6/src/main/java/christmas/domain/VisitDay.java
@@ -0,0 +1,39 @@
+package christmas.domain;
+
+import christmas.domain.constant.Day;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VisitDay {
+    private int day;
+
+    private static final List<Integer> specialDay = new ArrayList<>(List.of(3, 10, 17, 24, 25, 31));
+
+    public VisitDay(int day){
+        this.day = day;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public boolean isWeekend(){
+        return Day.of(day).isWeekend();
+    }
+
+    public boolean isWeekDay(){
+        return Day.of(day).isWeekDay();
+    }
+
+    public boolean isAfterChristmas(){
+        return this.day <= 25;
+    }
+
+    public int dDayOfChristmas(){
+        return this.day - 1;
+    }
+
+    public boolean isStarDay(){
+        return specialDay.contains(this.day);
+    }
+}
